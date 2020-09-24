8bf2fd6a6ae19e6700d6f51ed2677db33fae427d
Jc Dior <jessicaclairdior@gmail.com>
2020-09-24 02:29:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,8 +26,9 @@ def initialize(name)
   end
 
   def artist_name=(name)
-     if (self.artist.nil?)
-       self.artist = Artist.new(name)
+     self.artist = Artist.find_or_create_by_name
+     (name)
+     artist.add_song(self)
      end
    end
 
