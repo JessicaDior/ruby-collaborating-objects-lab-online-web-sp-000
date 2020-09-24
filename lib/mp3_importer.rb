25ba4145c6d90bf37e99672cf17adfb99f3237e4
Jc Dior <jessicaclairdior@gmail.com>
2020-09-24 02:21:19 +0000

    Automatically backed up by Learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,17 @@
+class MP3Importer
+  attr_accessor :path
+
+  def initialize(file_path)
+    @path = file_path
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.delete_if {|file| file == "." || file == ".."}
+  end
+
+  def import
+    files.each{ |filename| Song.files(filename) }
+  end
+
+end
